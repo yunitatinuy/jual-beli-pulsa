5ff453722dbe940fc7a56a0eea489ad068bae2d6
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2023-11-27 12:17:45 +0700

    Update index.php

--- PBL/index.php
+++ PBL/index.php
@@ -19,7 +19,7 @@
       <div class="collapse navbar-collapse text-right" id="navbarText">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="registrasi.html">Registration</a>
+            <a class="nav-link active" aria-current="page" href="registrasi.php">Registration</a>
           </li>
         </ul>
       </div>
