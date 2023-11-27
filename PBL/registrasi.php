bfc740072421dcb4122fc00de27295f48328085b
Muhammad Dafa Putra <muhammaddavaputra@gmail.com>
2023-11-27 12:18:51 +0700

    Update registrasi.php

--- PBL/registrasi.php
+++ PBL/registrasi.php
@@ -54,7 +54,7 @@ if (isset($_POST['submit'])) {
       <div class="collapse navbar-collapse text-right" id="navbarText">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html">Login</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Login</a>
           </li>
         </ul>
       </div>
