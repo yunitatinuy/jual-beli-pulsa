e0df4930ce484bb7a509523a864f4db0fbfd53db
Yunita Caroline <150452359+yunitatinuy@users.noreply.github.com>
2023-12-01 02:15:01 +0700

    update landing page

--- PBL/selengkapnya.php
+++ PBL/selengkapnya.php
@@ -1,89 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Jual Beli Pulsa</title> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="style2.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jual Beli Pulsa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
+
 <body id="home">
-<nav>
-  <div class="wrapper">
-    <div class="logo"><a href="">QUICK.TOP</a></div>
-    <div class="menu">
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#desc">Deskripsi</a></li>
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-        <li><a href="registrasi.php" class="tbl-green">Registration</a></li>
-      </ul>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href="">QUICK.TOP</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#desc">Description</a></li>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                    <li><a href="registrasi.php" class="tbl-green">Registration</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+            <!--home-->
+            <img src="c1.jpg" width="400px" height="400px">
+            <div class="kolom">
+                <br>
+                <br>
+                <p class="deskripsi">QUICK.TOP</p>
+                <h2>Website Jual Beli Pulsa</h2>
+                <p>Termurah dan terpercaya! Untuk urusan pulsa all operator, tak perlu bayar mahal. Jadilah pelanggan
+                    kami dan dapatkan harga terbaik serta kesempatan bonus bagi anda yang jadi pelanggan setia kami</p>
+            </div>
+        </section>
+
+        <!--deskripsi-->
+        <section id="desc">
+            <div class="kolom">
+                <p class="deskripsi"></p>
+                <h2>Description</h2>
+                <p>Pulsa sudah menjadi bagian dari kebutuhan yang wajib dipenuhi karena tanpa adanya pulsa akan sulit
+                    bagi seseorang dalam menjalankan fungsi ponselnya dengan maksimal. Untuk itu, aplikasi ini dirancang
+                    agar mempermudah pembeli untuk dapat membeli pulsa secara online tanpa harus mendatangi konter
+                    penjualan pulsa.</p>
+                <p>Pulsa sudah menjadi bagian dari kebutuhan yang wajib dipenuhi karena tanpa adanya pulsa akan sulit
+                    bagi seseorang dalam menjalankan fungsi ponselnya dengan maksimal. Untuk itu, aplikasi ini dirancang
+                    agar mempermudah pembeli untuk dapat membeli pulsa secara online tanpa harus mendatangi konter
+                    penjualan pulsa.</p>
+                <p>Keuntungan dari aplikasi ini adalah waktu pembeliannya yang fleksibel. Seperti contoh, ketika dalam
+                    keadaan darurat dan membutuhkan pulsa, sedangkan konter penjualan pulsa sudah tutup. Oleh karena itu
+                    melalui aplikasi ini, pengguna dapat mengakses layanan pengisian pulsa selama 24 jam dan tidak lagi
+                    mengkhawatirkan kehabisan pulsa di saat darurat.</p>
+                <p>Aplikasi jual beli pulsa ini bertujuan untuk membangun platform digital yang memungkinkan pengguna
+                    untuk membeli pulsa dengan mudah dan nyaman. Tujuan dari pembuatan jual beli pulsa secara online :
+                <p>1. Membuat akses menjadi lebih muda</p>
+                <p>2. Meningkatkan kepuasan pelanggan</p>
+                <p>3. Pemantauan transaksi</p>
+            </div>
+            <img src="https://img.freepik.com/free-photo/3d-hand-making-cashless-payment-from-smartphone_107791-16609.jpg?size=626&ext=jpg&ga=GA1.1.2070552831.1701066348&semt=sph"
+                width="400px" height="400px">
+        </section>
+
+        <!--about us-->
+        <section id="about">
+            <img src="https://img.freepik.com/free-vector/body-language-concept-illustration_114360-19536.jpg?size=626&ext=jpg&ga=GA1.1.2070552831.1701066348&semt=ais"
+                width="400px" height="400px">
+            <div class="kolom">
+                <p class="deskripsi"></p>
+                <h2>About Us</h2>
+                <p>Kami merancang website ini sebagai bagian dari tugas Project Base Learning (PBL)</p>
+                <p><a href="about.php" class="tbl-hijau">Selengkapnya</a></p>
+            </div>
+        </section>
+
+        <!--contact us-->
+        <section id="contact">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>QUICK.TOP</h3>
+                    <p><img src="logo.jpeg" width="200" class="rounded-circle" alt="..." /></p>
+                </div>
+                <div class="footer-section">
+                    <h3>Contact</h3>
+                    <p>Jl. Jenderal Ahmad Yani</p>
+                    <p>Kode Pos : 123456</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social Media</h3>
+                    <p><i class="bi bi-envelope"></i> quick.top@gmail.com</p>
+                    <p><i class="bi bi-telephone"></i> 0000-0000-0000</p>
+                    <p><i class="bi bi-geo-alt-fill"></i> Batam, Indonesia</p>
+                </div>
+        </section>
     </div>
-  </div>
-</nav>
-  <div class="wrapper">
-    <section id="home">
-      <!--home-->
-      <img src="c1.jpg" width="400px" height="400px">
-      <div class="kolom">
-        <br>
-        <br>
-        <p class="deskripsi">QUICK.TOP</p>
-        <h2>Website Jual Beli Pulsa</h2>
-        <p>Termurah dan terpercaya! Untuk urusan pulsa all operator, tak perlu bayar mahal. Jadilah pelanggan kami dan dapatkan harga terbaik serta kesempatan bonus bagi anda yang jadi pelanggan setia kami</p>
-      </div>
-    </section>
 
-    <!--deskripsi-->
-    <section id="desc">
-      <div class="kolom">
-        <p class="deskripsi"></p>
-        <h2>Deskripsi</h2>
-        <p>Pulsa sudah menjadi bagian dari kebutuhan yang wajib dipenuhi karena tanpa adanya pulsa akan sulit bagi seseorang dalam menjalankan fungsi ponselnya dengan maksimal. Untuk itu, aplikasi ini dirancang agar mempermudah pembeli untuk dapat membeli pulsa secara online tanpa harus mendatangi konter penjualan pulsa.</p>
-        <p>Pulsa sudah menjadi bagian dari kebutuhan yang wajib dipenuhi karena tanpa adanya pulsa akan sulit bagi seseorang dalam menjalankan fungsi ponselnya dengan maksimal. Untuk itu, aplikasi ini dirancang agar mempermudah pembeli untuk dapat membeli pulsa secara online tanpa harus mendatangi konter penjualan pulsa.</p>
-        <p>Keuntungan dari aplikasi ini adalah waktu pembeliannya yang fleksibel. Seperti contoh, ketika dalam keadaan darurat dan membutuhkan pulsa, sedangkan konter penjualan pulsa sudah tutup. Oleh karena itu melalui aplikasi ini, pengguna dapat mengakses layanan pengisian pulsa selama 24 jam dan tidak lagi mengkhawatirkan kehabisan pulsa di saat darurat.</p>
-        <p>Aplikasi jual beli pulsa ini bertujuan untuk membangun platform digital yang memungkinkan pengguna untuk membeli pulsa dengan mudah dan nyaman. Tujuan dari pembuatan jual beli pulsa secara online :
-        <p>1. Membuat akses menjadi lebih muda</p>
-        <p>2. Meningkatkan kepuasan pelanggan</p>
-        <p>3. Pemantauan transaksi</p>
-      </div>
-      <img src="https://img.freepik.com/free-photo/3d-hand-making-cashless-payment-from-smartphone_107791-16609.jpg?size=626&ext=jpg&ga=GA1.1.2070552831.1701066348&semt=sph" width="400px" height="400px">
-    </section>
-
-    <!--about us-->
-    <section id="about">
-    <img src="https://img.freepik.com/free-vector/body-language-concept-illustration_114360-19536.jpg?size=626&ext=jpg&ga=GA1.1.2070552831.1701066348&semt=ais" width="400px" height="400px">
-      <div class="kolom">
-        <p class="deskripsi"></p>
-        <h2>About Us</h2>
-        <p>Kami merancang website ini sebagai bagian dari tugas Project Base Learning (PBL)</p>
-        <p><a href="about.php" class="tbl-hijau">Selengkapnya</a></p>
-      </div>
-    </section>
-
-    <!--contact us-->
-    <section id="contact">
-      <div class="footer">
-        <div class="footer-section">
-          <h3>QUICK.TOP</h3>
-          <p><img src="logo.jpeg" width="200" class="rounded-circle" alt="..." /></p>
-        </div>
-        <div class="footer-section">
-          <h3>Contact</h3>
-          <p>Jl. Jenderal Ahmad Yani</p>
-          <p>Kode Pos : 123456</p>
-        </div>
-        <div class="footer-section">
-          <h3>Social Media</h3>
-          <p><i class="bi bi-envelope"></i> quick.top@gmail.com</p>
-          <p><i class="bi bi-telephone"></i> 0000-0000-0000</p>
-          <p><i class="bi bi-geo-alt-fill"></i> Batam, Indonesia</p>
-        </div>
-    </section>
-  </div>
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
