1521c902b2886cddb25704290cd46238b8188276
yunitatinuy <yunitacaroline288@gmail.com>
2023-12-23 20:32:10 +0700

    database

--- PBL/db_admin.sql
+++ PBL/db_admin.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2023 at 06:30 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 23 Des 2023 pada 14.15
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Struktur dari tabel `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -33,7 +33,7 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `kategori`
+-- Dumping data untuk tabel `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
@@ -42,7 +42,7 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `metode_bayar`
+-- Struktur dari tabel `metode_bayar`
 --
 
 CREATE TABLE `metode_bayar` (
@@ -52,7 +52,7 @@ CREATE TABLE `metode_bayar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `metode_bayar`
+-- Dumping data untuk tabel `metode_bayar`
 --
 
 INSERT INTO `metode_bayar` (`id_metode_bayar`, `jenis`, `kode`) VALUES
@@ -62,7 +62,7 @@ INSERT INTO `metode_bayar` (`id_metode_bayar`, `jenis`, `kode`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nohp`
+-- Struktur dari tabel `nohp`
 --
 
 CREATE TABLE `nohp` (
@@ -72,23 +72,20 @@ CREATE TABLE `nohp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `nohp`
+-- Dumping data untuk tabel `nohp`
 --
 
 INSERT INTO `nohp` (`id_nohp`, `id_provider`, `no_telp`) VALUES
-(94, 7, '083181531047'),
-(95, 8, '082382467889'),
-(96, 9, '1352361623'),
-(97, 20, '56136237124'),
-(98, 8, '1351155112'),
-(99, 10, '16136134'),
-(100, 9, '083181531047'),
-(101, 20, '083181531047');
+(114, 7, '1231546'),
+(115, 7, '13246546'),
+(116, 7, '0988777'),
+(117, 7, '12315'),
+(118, 7, '1214656565');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `proveder`
+-- Struktur dari tabel `proveder`
 --
 
 CREATE TABLE `proveder` (
@@ -102,24 +99,35 @@ CREATE TABLE `proveder` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `proveder`
+-- Dumping data untuk tabel `proveder`
 --
 
 INSERT INTO `proveder` (`id_provider`, `id_kategori`, `nama_provider`, `detail`, `nominal`, `harga`, `foto`) VALUES
-(6, 1, 'Indosat', 'Masa aktif 30 hari + gratis nelpon 120mnt', 20000, 22000, 'indosat.png'),
-(7, 1, 'Telkomsel', 'Masa aktif 10 hari + Gratis Nelpon 120mnt', 15000, 17000, 'telkomsel.png'),
-(8, 1, 'XL', 'Masa aktif 3 hari + gratis sms 100 kali', 5000, 7000, 'xl.png'),
-(9, 1, 'Axis', 'gratis nelpon ke sesama', 10000, 12000, 'axis.png'),
-(10, 1, 'Telkomsel', 'gratis sms', 10000, 12000, 'telkomsel.png'),
-(13, 1, 'Telkomsel', 'gratis 1GB internet + telpon kesesama operator', 20000, 22000, 'telkomsel.png'),
-(17, 1, 'Indosat', 'Masa aktif 15 hari + Gratis Nelpon 150mnt', 25000, 27000, 'indosat.png'),
-(19, 1, 'Telkomsel', 'gratis sms', 5000, 7000, 'telkomsel.png'),
-(20, 1, 'Axis', 'gratis nelpon kesesama', 5000, 7000, 'axis.png');
+(1, 1, 'Telkomsel', 'Masa Aktif 7 hari', 5000, 7000, 'telkomsel.png'),
+(2, 1, 'Telkomsel', 'Masa Aktif 10 hari', 10000, 12000, 'telkomsel.png'),
+(3, 1, 'Telkomsel', 'Masa Aktif 30 hari', 20000, 22000, 'telkomsel.png'),
+(4, 1, 'Telkomsel', 'Masa Aktif 45 hari + 30 Hari Prime Video', 50000, 52000, 'telkomsel.png'),
+(5, 1, 'XL', 'Masa Aktif 7 hari + Kuota 1 GB', 5000, 7000, 'xl.png'),
+(6, 1, 'XL', 'Masa Aktif 15 hari + Kuota 2 GB', 10000, 12000, 'xl.png'),
+(7, 1, 'XL', 'Masa Aktif 45 hari ', 30000, 32000, 'xl.png'),
+(8, 1, 'XL', 'Masa Aktif 45 hari + Kuota 2 GB', 50000, 52000, 'xl.png'),
+(9, 1, 'Axis', 'Bonus Isi Pulsa 200 MB 1 hari', 5000, 7000, 'axis.png'),
+(10, 1, 'Axis', 'Bonus Isi Pulsa 500 MB 3 hari', 10000, 12000, 'axis.png'),
+(11, 1, 'Axis', 'Bonus Isi Pulsa 1,5 GB 3 hari', 30000, 32000, 'axis.png'),
+(12, 1, 'Axis', 'Bonus Isi Pulsa 3 GB 3 hari', 50000, 52000, 'axis.png'),
+(13, 1, 'Indosat', 'Masa Aktif 7 hari', 5000, 7000, 'indosat.png'),
+(14, 1, 'Indosat', 'Masa Aktif 15 hari', 10000, 12000, 'indosat.png'),
+(15, 1, 'Indosat', 'Masa Aktif 30 hari + Gratis Game Sepuasnya 1 hari', 25000, 27000, 'indosat.png'),
+(16, 1, 'Indosat', 'Masa Aktif 30 hari + Streaming Netflix, Viu, IQIYI 3 hari', 50000, 55000, 'indosat.png'),
+(17, 1, 'Tri', 'Masa Aktif 5 hari', 5000, 7000, 'tri.png'),
+(18, 1, 'Tri', 'Masa Aktif 10 hari', 10000, 12000, 'tri.png'),
+(19, 1, 'Tri', 'Masa Aktif 30 hari + Gratis Telpon & SMS', 30000, 32000, 'tri.png'),
+(20, 1, 'Tri', 'Masa Aktif 30 hari + Gratis Internetan 5 hari', 50000, 52000, 'tri.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi`
+-- Struktur dari tabel `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -130,41 +138,24 @@ CREATE TABLE `transaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `transaksi`
+-- Dumping data untuk tabel `transaksi`
 --
 
 INSERT INTO `transaksi` (`id_transaksi`, `id_transaksi_penjualan`, `id_provider`, `jumlah`) VALUES
-(25, 25, 7, 1),
-(26, 26, 7, 1),
-(27, 27, 17, 1),
-(28, 28, 7, 1),
-(29, 29, 9, 1),
-(30, 30, 7, 1),
-(31, 31, 8, 1),
-(32, 32, 7, 1),
-(33, 33, 19, 1),
-(34, 34, 8, 1),
-(35, 35, 7, 1),
-(36, 36, 9, 1),
-(37, 37, 8, 1),
-(38, 38, 8, 1),
-(39, 39, 7, 1),
-(40, 40, 8, 1),
-(41, 41, 9, 1),
-(42, 42, 20, 1),
-(43, 43, 8, 1),
-(44, 44, 10, 1),
-(45, 45, 9, 2),
-(46, 46, 20, 1);
+(54, 54, 20, 2),
+(55, 55, 7, 1),
+(56, 56, 7, 1),
+(57, 57, 7, 1),
+(58, 57, 20, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi_penjualan`
+-- Struktur dari tabel `transaksi_penjualan`
 --
 
 CREATE TABLE `transaksi_penjualan` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi_penjualan` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_nohp` int(11) NOT NULL,
   `id_metode_bayar` int(11) NOT NULL,
@@ -173,19 +164,20 @@ CREATE TABLE `transaksi_penjualan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `transaksi_penjualan`
+-- Dumping data untuk tabel `transaksi_penjualan`
 --
 
-INSERT INTO `transaksi_penjualan` (`id_transaksi`, `id_user`, `id_nohp`, `id_metode_bayar`, `tanggal_pembelian`, `total_pembelian`) VALUES
-(43, 35, 98, 1, '2023-12-20', 22000),
-(44, 35, 99, 1, '2023-12-20', 22000),
-(45, 34, 100, 2, '2023-12-20', 44000),
-(46, 34, 101, 1, '2023-12-21', 22000);
+INSERT INTO `transaksi_penjualan` (`id_transaksi_penjualan`, `id_user`, `id_nohp`, `id_metode_bayar`, `tanggal_pembelian`, `total_pembelian`) VALUES
+(54, 34, 109, 2, '2023-12-21', 14000),
+(55, 34, 110, 1, '2023-12-21', 17000),
+(56, 34, 112, 1, '2023-12-21', 17000),
+(57, 34, 116, 0, '2023-12-21', 17000),
+(58, 34, 116, 0, '2023-12-21', 7000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -198,7 +190,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `username`, `email`, `password`, `role`) VALUES
@@ -206,99 +198,100 @@ INSERT INTO `user` (`id_user`, `nama`, `username`, `email`, `password`, `role`) 
 (30, 'Yunita Caroline Sianturi', 'tinuy', 'tinuy@gmail.com', '$2y$10$s50dtb0J9Fe/qmRIQbbN5.JBsm0D855LORTXNnfRfAaPNOeG/NmBm', 0),
 (33, 'Muhammad Dafa Putra', 'admin', 'muhammaddavaputra@gmail.com', '$2y$10$VoIF/JU.vKEV5Ym6ZHU6Oetr.Qg/423vIUM0X9Inz7RC4mxGjuGUq', 1),
 (34, 'mizu', 'user', 'rkiyoshi13@gmail.com', '$2y$10$6U4l4/5mZ2wNWgIf2hEO7ONf2UtqE4gq/l/bM2g5DHvDQMjEJ..3O', 0),
-(35, 'dhaput', 'dhaput22', 'rkiyoshi13@gmail.com', '$2y$10$SZtqk7QhXSTzTCCgMyKWeuEWXwgJazt6xaumUGfZXzHElYzSFWMV.', 0);
+(35, 'dhaput', 'dhaput22', 'rkiyoshi13@gmail.com', '$2y$10$SZtqk7QhXSTzTCCgMyKWeuEWXwgJazt6xaumUGfZXzHElYzSFWMV.', 0),
+(36, 'mikasa', 'mikasa', 'mikasa@gmail.com', '$2y$10$HZRuAebAv.UrCeWieAn6uerD9Mu1p0RYgvCAvk7t6fyToPya2lGrm', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `kategori`
+-- Indeks untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `metode_bayar`
+-- Indeks untuk tabel `metode_bayar`
 --
 ALTER TABLE `metode_bayar`
   ADD PRIMARY KEY (`id_metode_bayar`);
 
 --
--- Indexes for table `nohp`
+-- Indeks untuk tabel `nohp`
 --
 ALTER TABLE `nohp`
   ADD PRIMARY KEY (`id_nohp`);
 
 --
--- Indexes for table `proveder`
+-- Indeks untuk tabel `proveder`
 --
 ALTER TABLE `proveder`
   ADD PRIMARY KEY (`id_provider`);
 
 --
--- Indexes for table `transaksi`
+-- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indexes for table `transaksi_penjualan`
+-- Indeks untuk tabel `transaksi_penjualan`
 --
 ALTER TABLE `transaksi_penjualan`
-  ADD PRIMARY KEY (`id_transaksi`);
+  ADD PRIMARY KEY (`id_transaksi_penjualan`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `kategori`
+-- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `metode_bayar`
+-- AUTO_INCREMENT untuk tabel `metode_bayar`
 --
 ALTER TABLE `metode_bayar`
   MODIFY `id_metode_bayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT for table `nohp`
+-- AUTO_INCREMENT untuk tabel `nohp`
 --
 ALTER TABLE `nohp`
-  MODIFY `id_nohp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id_nohp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
--- AUTO_INCREMENT for table `proveder`
+-- AUTO_INCREMENT untuk tabel `proveder`
 --
 ALTER TABLE `proveder`
-  MODIFY `id_provider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_provider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
--- AUTO_INCREMENT for table `transaksi`
+-- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
--- AUTO_INCREMENT for table `transaksi_penjualan`
+-- AUTO_INCREMENT untuk tabel `transaksi_penjualan`
 --
 ALTER TABLE `transaksi_penjualan`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id_transaksi_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
